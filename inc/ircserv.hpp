1eb57da57ec4d9a2f0f2ed77161b7c5de20623d5
oussama <zhdosm@gmail.com>
2023-06-25 11:58:11 +0100

    Feat: start coding server

--- inc/ircserv.hpp
+++ inc/ircserv.hpp
@@ -6,12 +6,14 @@
 /*   By: ozahid- <ozahid-@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 11:57:39 by junik             #+#    #+#             */
-/*   Updated: 2023/06/24 15:21:48 by ozahid-          ###   ########.fr       */
+/*   Updated: 2023/06/25 11:56:59 by ozahid-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <exception>
+#include <stdexcept>
+#include <sys/socket.h>
 
 int	handle_port(std::string port_);
 bool	handle_password(std::string password_);
---
+++ src/server/server.cpp
@@ -0,0 +1,2 @@
+#include "ircserv.hpp"
+
